40e68c34b2be3c4a05bd5875df92b7c3aad6ff2c
Hannapham1007 <hannaphamm1007@gmail.com>
2024-07-05 00:08:55 +0200

    added job dto

---
+++ src/main/java/com/springboot/indeedclone/dto/JobDTO.java
@@ -0,0 +1,21 @@
+package com.springboot.indeedclone.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class JobDTO {
+    private int id;
+    private String title;
+    private String description;
+    private String company;
+    private String location;
+    private String type;
+    private List<String> technology;
+    private String experience;
+    private String salary;
+
+}
